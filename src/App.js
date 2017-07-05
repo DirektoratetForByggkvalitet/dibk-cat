96b193e18ad0e4a08cd5c8da0d443b59b34e0521
Kristoffer Brabrand <kristoffer@brabrand.no>
2017-07-05 16:59:38 +0200

    Merge

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
+import { Provider } from 'react-redux';
+
 import { Wizard } from "dibk-wizard-framework";
 
 import cat from "./api/cat";
+import store from './store';
 
 class App extends Component {
   render() {
-    return <Wizard schema={cat} />;
+    return (
+      <Provider store={store}>
+        <Wizard schema={cat} />
+      </Provider>
+    )
   }
 }
 
