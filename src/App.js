9b6565f6200eadb63c230b88b663414f67c3bdbb
Kristoffer Brabrand <kristoffer@brabrand.no>
2017-07-05 12:55:50 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Wizard } from 'dibk-wizard-framework'
+
+import config from './config'
+
+class App extends Component {
+  render() {
+    return <Wizard />
+  }
+}
+
+export default App;
