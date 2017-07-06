ed4655ef5f4f6649d20d856b616a6354e1403958
Anders Olsen Sandvik <github@andersos.net>
2017-07-06 22:47:26 +0200

    Lint project

--- src/store.js
+++ src/store.js
@@ -1,12 +1,10 @@
-import { createStore, combineReducers, compose } from 'redux'
-import { state } from 'dibk-wizard-framework'
+import { createStore, combineReducers, compose } from 'redux';
+import { state } from 'dibk-wizard-framework';
 
 /**
  * Create the store with middleware
  */
 export default createStore(
   combineReducers({ [state.NAME]: state.reducer }),
-  compose(
-    window.devToolsExtension ? window.devToolsExtension() : f => f,
-  )
-)
+  compose(window.devToolsExtension ? window.devToolsExtension() : f => f) /* eslint no-undef: 0 */,
+);
