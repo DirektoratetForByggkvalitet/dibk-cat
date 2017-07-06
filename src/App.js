8dd1e99bfb2e58672d259dbd5edae392fdc58034
Kristoffer Brabrand <kristoffer@brabrand.no>
2017-07-06 16:26:30 +0200

    Fix path for altis font

--- src/App.js
+++ src/App.js
@@ -10,22 +10,22 @@ import store from './store';
 injectGlobal`
   @font-face {
       font-family: 'Altis';
-      src: url('altis-light.eot');
-      src: url('altis-light.eot?#iefix') format('embedded-opentype'),
-           url('altis-light.woff2') format('woff2'),
-           url('altis-light.woff') format('woff'),
-           url('altis-light.ttf') format('truetype');
+      src: url('/fonts/altis-light.eot');
+      src: url('/fonts/altis-light.eot?#iefix') format('embedded-opentype'),
+           url('/fonts/altis-light.woff2') format('woff2'),
+           url('/fonts/altis-light.woff') format('woff'),
+           url('/fonts/altis-light.ttf') format('truetype');
       font-weight: light;
       font-style: normal;
   }
 
   @font-face {
       font-family: 'Altis';
-      src: url('altis-medium.eot');
-      src: url('altis-medium.eot?#iefix') format('embedded-opentype'),
-           url('altis-medium.woff2') format('woff2'),
-           url('altis-medium.woff') format('woff'),
-           url('altis-medium.ttf') format('truetype');
+      src: url('/fonts/altis-medium.eot');
+      src: url('/fonts/altis-medium.eot?#iefix') format('embedded-opentype'),
+           url('/fonts/altis-medium.woff2') format('woff2'),
+           url('/fonts/altis-medium.woff') format('woff'),
+           url('/fonts/altis-medium.ttf') format('truetype');
       font-weight: light;
       font-style: normal;
   }
