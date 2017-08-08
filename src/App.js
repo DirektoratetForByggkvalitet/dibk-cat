09a4f2b12bb0b897cacdaac0d697d2b0ee836522
Anders Olsen Sandvik <github@andersos.net>
2017-08-08 16:29:11 +0200

    Use pathfinder

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 
 import { Wizard } from 'dibk-wizard-framework';
-
-import cat from './api/cat';
 import store from './store';
 
-export default function App() {
-  return (
-    <Provider store={store}>
-      <Wizard schema={cat} />
-    </Provider>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      json: '',
+    };
+  }
+  componentDidMount() {
+    /* eslint-disable */
+    fetch('https://oracle-ea1f3.firebaseio.com/trees/-KqweqqKlwN3_3CRlbct.json')
+      /* eslint-enable */
+      .then((response) => {
+        const contentType = response.headers.get('content-type');
+        if (contentType && contentType.includes('application/json')) {
+          return response.json();
+        }
+        throw new TypeError("Oops, we haven't got JSON!");
+      })
+      .then((json) => {
+        this.setState({
+          loading: false,
+          json,
+        });
+      })
+      .catch((error) => {
+        console.log(error); /* eslint no-console: 0*/
+      });
+  }
+  render() {
+    if (!this.state.loading) {
+      return (
+        <Provider store={store}>
+          <Wizard schema={this.state.json} />
+        </Provider>
+      );
+    }
+    return <p>Loading...</p>;
+  }
 }
