e52867b4db386a7ea753c2c12822019100c97225
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2017-08-25 15:04:34 +0200

    Consistent types

--- src/api/cat.js
+++ src/api/cat.js
@@ -277,27 +277,28 @@ export default {
       children: [
         {
           property: 'favourite.actor',
-          type: 'input',
+          type: 'Input',
           heading: 'Skrive navnet på favoritt skuespilleren?',
+          text: 'Input burde også ha noe brødtekst ikkesant?',
           placeholder: 'Kit Harington',
         },
         {
           property: 'behavior.with.animals',
-          type: 'textarea',
+          type: 'Textarea',
           heading: 'Skriv litt om hvordan du er med dyr?',
           placeholder: 'Hvordan oppfører du deg rundt dyr',
         },
         {
           property: 'cat.information',
-          type: 'text',
-          heading: 'Dette er en informasjonsblokk?',
-          body:
+          type: 'Text',
+          heading: 'Dette er en textblokk',
+          text:
             "Cat ipsum dolor sit amet, always hungry man running from cops stops to pet cats, goes to jail step on your keyboard while you're gaming and then turn in a circle . Pose purrfectly to show my beauty woops poop hanging from butt must get rid run run around house drag poop on floor maybe it comes off woops left brown marks on floor human slave clean lick butt now but lick plastic bags destroy couch, yet hunt by meowing loudly at 5am next to human slave food dispenser. Sleep nap meow meow or put toy mouse in food bowl run out of litter box at full speed but meow all night and hunt anything that moves. My left donut is missing, as is my right. Rub whiskers on bare skin act innocent shove bum in owner's face like camera lens but munch on tasty moths meow for meow all night. Need to chase tail massacre a bird in the living room and then look like the cutest and most innocent animal on the planet. ",
         },
         {
           property: 'cat.image',
-          type: 'image',
-          text: 'Dette er en informasjonsblokk?',
+          type: 'Image',
+          text: 'Dette er en bildeblokk',
           url: 'https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif',
         },
       ],
