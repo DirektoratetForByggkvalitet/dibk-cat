198e9379738aa0d86a6e1f3b44f1767407a9030d
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2017-09-01 16:32:52 +0200

    Add styled components

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { H1 } from '../primitives/Heading';
 
 export default function Intro({ close }) {
   return (
     <div>
+      <H1>Pink text from styled components</H1>
       <p>Welcome to the intro page</p>
       <button onClick={() => close()}>
         Start the magic{' '}
