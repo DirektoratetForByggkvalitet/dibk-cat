9e73de730d1d9c359e87b4387246fd4be8e07e78
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2017-09-01 12:22:31 +0200

    No longer use pathfinder

--- src/App.js
+++ src/App.js
@@ -1,47 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 
 import { Wizard } from 'dibk-wizard-framework';
 import store from './store';
-// import cat from './api/cat';
+import cat from './api/cat';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true,
-      json: '',
-    };
-  }
-  componentDidMount() {
-    /* eslint-disable */
-    fetch('https://oracle-ea1f3.firebaseio.com/trees/-KqweqqKlwN3_3CRlbct/dibk.json')
-      /* eslint-enable */
-      .then((response) => {
-        const contentType = response.headers.get('content-type');
-        if (contentType && contentType.includes('application/json')) {
-          return response.json();
-        }
-        throw new TypeError("Oops, we haven't got JSON!");
-      })
-      .then((json) => {
-        this.setState({
-          loading: false,
-          json,
-        });
-      })
-      .catch((error) => {
-        console.log(error); /* eslint no-console: 0*/
-      });
-  }
-  render() {
-    if (!this.state.loading) {
-      return (
-        <Provider store={store}>
-          <Wizard wizard={this.state.json} />
-        </Provider>
-      );
-    }
-    return <p>Loading...</p>;
-  }
+export default function App() {
+  return (
+    <Provider store={store}>
+      <Wizard wizard={cat} />
+    </Provider>
+  );
 }
