f25991d336e28233bae64cc069b9690695bd9ed3
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2017-09-08 11:02:26 +0200

    Fix #16 add redux-persist

--- src/store.js
+++ src/store.js
@@ -1,10 +1,18 @@
 import { createStore, combineReducers, compose } from 'redux';
 import { state } from 'dibk-wizard-framework';
+import { persistStore, autoRehydrate } from 'redux-persist';
 
 /**
  * Create the store with middleware
  */
-export default createStore(
+const store = createStore(
   combineReducers({ [state.NAME]: state.reducer }),
-  compose(window.devToolsExtension ? window.devToolsExtension() : f => f) /* eslint no-undef: 0 */,
+  compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f,
+    autoRehydrate(),
+  ) /* eslint no-undef: 0 */,
 );
+
+persistStore(store);
+
+export default store;
