c2cac8260fd8e2b4b3ca1e544ea6f24390f6eece
Kristoffer Brabrand <kristoffer@brabrand.no>
2017-09-15 11:26:34 +0200

    Use h1 primitive

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { H1 } from '../primitives/Heading';
 import { Primitives } from 'dibk-wizard-framework';
 
 export default function Intro({ close }) {
   return (
     <div>
-      <H1>Pink text from styled components</H1>
+      <Primitives.Heading.H1>Pink text from styled components</Primitives.Heading.H1>
       <p>Welcome to the intro page</p>
       <Primitives.Button onClick={() => close()}>
         Start the magic{' '}
